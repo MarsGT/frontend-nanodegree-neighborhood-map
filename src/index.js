e19157884aaeadf68185c7e588707b77f8ab2cf5
Liang-hsiao <marsgt@qq.com>
2019-05-06 18:49:33 +0800

    chore: 文件夹整理

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './style/index.css';
 import App from './components/App';
-import * as serviceWorker from './tool/sw';
+import * as serviceWorker from './utils/sw';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
